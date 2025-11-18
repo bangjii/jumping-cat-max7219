1b09fc4b99e95ca8f5f34033d7eb724421fd6c59
Hudzaifah Al Jihad <al.ghifari22@gmail.com>
2025-11-18 13:45:50 +0700

    Refactor image drawing and add animation function

--- jumping-cat-max7219.ino
+++ jumping-cat-max7219.ino
@@ -9,24 +9,35 @@
 
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 
+// ----- FRAME ANIMASI -----
 const uint64_t IMAGES[] = {
-  0x9c787ce060101000,
   0x8e7c3e30f0101000,
   0x077e1f1878081000,
   0x8e7c3e30f0080800,
-  0x9c787ce060100800
+  0x9c787ce060100800,
+  0x9c787ce060101000,
+  0x9c787ce060201000
 };
 const int IMAGES_LEN = sizeof(IMAGES)/8;
 
-void drawImageNoFlicker(uint64_t img) {
-  mx.control(MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);  // matikan refresh sementara
+// ----- DRAW TANPA FLICKER -----
+void drawImageNoFlicker(uint8_t device, uint64_t img) {
+  mx.control(MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
 
   for (int col = 0; col < 8; col++) {
     uint8_t colData = (img >> (col * 8)) & 0xFF;
-    mx.setColumn(0, col, colData);
+    mx.setColumn(device, col, colData);
   }
 
-  mx.control(MD_MAX72XX::UPDATE, MD_MAX72XX::ON);   // hidupkan refresh (langsung tampil)
+  mx.control(MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
+}
+
+// ----- FUNGSI ANIMASI -----
+void catJump(uint8_t device, uint16_t frameDelay) {
+  for (int i = 0; i < IMAGES_LEN; i++) {
+    drawImageNoFlicker(device, IMAGES[i]);
+    delay(frameDelay);
+  }
 }
 
 void setup() {
@@ -36,9 +47,5 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < IMAGES_LEN; i++) {
-    drawImageNoFlicker(IMAGES[i]);
-    delay(200);
-  }
-  //delay(3000);
+  catJump(2, 200);
 }
